9a2194aa9fcec19c60e92e8e0278a9631d990431
ranimAlmansour <renimelmansur2244@gmail.com>
2024-12-04 11:06:40 +0300

    Add project files.

---
+++ GuzellikSalonu/Controllers/AccountController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NETCore.Encrypt.Extensions;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Security.Claims;
+using GuzellikSalonu.Entities;
+using GuzellikSalonu.Models;
+
+namespace GuzellikSalonu.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(DatabaseContext databaseContext, IConfiguration configuration)
+        {
+            _databaseContext = databaseContext;
+            _configuration = configuration;
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();  
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+                string saltedPassword = model.Password + md5Salt;
+                string hashedPassword = saltedPassword.MD5();
+
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower() && x.Password == hashedPassword);
+                if(user!=null)
+                {
+                    if(user.Locked)
+                    {
+                        ModelState.AddModelError(nameof(model.Username), "User is locked.");
+                        return View(model);
+                    }
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name, user.FullName ?? string.Empty));
+                    claims.Add(new Claim(ClaimTypes.Role, user.Role));
+
+                    //claims.Add(new Claim(ClaimTypes.Role, user.Role));
+                    claims.Add(new Claim("Username", user.Username));
+
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password is incorrect.");
+                }
+
+            }
+            return View(model);
+        }
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_databaseContext.Users.Any(x=>x.Username.ToLower()==model.Username.ToLower()))
+                {
+
+                    ModelState.AddModelError(nameof(model.Username), "Username is already exists.");
+                    View(model);
+                }
+                string md5Salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+                string saltedPassword = model.Password + md5Salt;
+                string hashedPassword = saltedPassword.MD5();
+                User user = new()
+                {
+                    Username = model.Username,
+                    Password = hashedPassword
+                };
+                _databaseContext.Users.Add(user);
+                int affectedRowCount = _databaseContext.SaveChanges();
+                if (affectedRowCount==0)
+                {
+                    ModelState.AddModelError("", "User can not be added.");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            return View(model);
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ProfileChangeFullName( string fullname)
+        {
+           
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
---
+++ GuzellikSalonu/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using GuzellikSalonu.Models;
+
+namespace GuzellikSalonu.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        /* private readonly ILogger<HomeController> _logger;
+
+         public HomeController(ILogger<HomeController> logger)
+         {
+            _logger = logger;
+         } */
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
